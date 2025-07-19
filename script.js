6c366c8519a61339da4ada2565c1a712ab3ffe46
Vinnesiaaa <vinn.nesiaa.id@gmail.com>
2025-07-19 07:46:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function generateQR() {
     const input = document.getElementById('qrInput').value;
     const qrCodeDiv = document.getElementById('qrCode');
     const downloadBtn = document.getElementById('downloadBtn');
+    const downloadCodeBtn = document.getElementById('downloadCodeBtn');
     const errorMsg = document.getElementById('errorMsg');
     const fgColor = document.getElementById('fgColor').value;
     const bgColor = document.getElementById('bgColor').value;
@@ -13,6 +14,7 @@ function generateQR() {
     errorMsg.classList.add('hidden');
     qrCodeDiv.innerHTML = '';
     downloadBtn.classList.add('hidden');
+    downloadCodeBtn.classList.add('hidden');
 
     if (!input) {
         errorMsg.textContent = 'Please enter text or URL!';
@@ -37,6 +39,7 @@ function generateQR() {
             correctLevel: QRCode.CorrectLevel.H
         });
         downloadBtn.classList.remove('hidden');
+        downloadCodeBtn.classList.remove('hidden');
     } catch (e) {
         errorMsg.textContent = 'Error generating QR code: ' + e.message;
         errorMsg.classList.remove('hidden');
@@ -54,6 +57,43 @@ function downloadQR() {
     } else {
         alert('No QR code to download. Generate one first!');
     }
+}
+
+function downloadCode() {
+    const input = document.getElementById('qrInput').value;
+    const fgColor = document.getElementById('fgColor').value;
+    const bgColor = document.getElementById('bgColor').value;
+    const qrSize = document.getElementById('qrSize').value;
+
+    const code = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>QR Code</title>
+    <script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
+</head>
+<body>
+    <div id="qrcode"></div>
+    <script>
+        new QRCode(document.getElementById("qrcode"), {
+            text: "${input}",
+            width: ${qrSize},
+            height: ${qrSize},
+            colorDark: "${fgColor}",
+            colorLight: "${bgColor}",
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    </script>
+</body>
+</html>
+    `;
+
+    const blob = new Blob([code], { type: 'text/html' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'qrcode-generator.html';
+    link.click();
 }
 
 document.getElementById('menuToggle').addEventListener('click', function() {
